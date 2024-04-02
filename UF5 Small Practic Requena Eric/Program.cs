a3bce04cf6511eeee3a705cb7570c9b201959d44
EricRequena <eric.requena.7e7@itb.cat>
2024-04-03 00:43:26 +0200

    Agregar archivos de proyecto.

---
+++ UF5 Small Practic Requena Eric/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using M03.UF5._AC1._Tipus_avançats_de_dades_en_C__Requena_Eric;
+
+namespace UF5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const int MAX_SCORES = 10;
+
+            List<Score> scores = new List<Score>();
+            
+            for (int i = 0; i < MAX_SCORES; i++)
+            {
+                Console.WriteLine("Enter player name: ");
+                string player = Console.ReadLine();
+                Console.WriteLine("Enter mission name: ");
+                string mission = Console.ReadLine();
+                Console.WriteLine("Enter score: ");
+                int score = Convert.ToInt32(Console.ReadLine());
+                Score s = new Score(player, mission, score);
+                if (s.Player == null || s.Mission == null || !(score >= 0 && score <= 500))
+                {
+                    i--;
+                }
+                else
+                {   
+                     scores.Add(s);
+                }
+            }/*
+            scores.Add(new Score("Laura", "Delta-001", 120));
+            scores.Add(new Score("Hugo", "Delta-002", 150));
+            scores.Add(new Score("Laura", "Delta-001", 135));
+            scores.Add(new Score("Tomas", "Delta-001", 200));
+            scores.Add(new Score("Laura", "Delta-002", 175));*/
+            
+            List<Score> uniqueScores = GenerateUniqueScores(scores);
+            uniqueScores.Sort();
+            foreach (Score s in uniqueScores)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        public static List<Score> GenerateUniqueScores(List<Score> scores)
+        {
+            // En la lista de scores, agrupamos por jugador y misión, y seleccionamos el máximo score de cada mision
+            var uniqueScores = from s in scores 
+                               group s by new { s.Player, s.Mission } into g
+                               select new Score(g.Key.Player, g.Key.Mission, g.Max(x => x.Scoring));
+            return uniqueScores.ToList();
+
+        }
+    }
+}
---
+++ UF5 Small Practic Requena Eric/Score.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace M03.UF5._AC1._Tipus_avançats_de_dades_en_C__Requena_Eric
+{
+    public class Score : IComparable<Score>
+    {
+        private string player;
+        private string mission;
+        private int scoring;
+
+        public Score(string player, string mission, int scoring)
+        {
+            this.Player = player;
+            this.Mission = mission;
+            this.Scoring = scoring;
+        }
+
+        public string Player
+        {
+            get { return player; }
+            set
+            {
+                if (Regex.IsMatch(value.Trim(), @"^[A-Za-z]+$"))
+                {
+                    player = value;
+                }
+                else
+                {
+                    Console.WriteLine("Player name must contain only alphabetic characters");
+                }
+            }
+        }
+
+        public string Mission
+        {
+            get { return mission; }
+            set
+            {
+
+                if (Regex.IsMatch(value.Trim(), @"^(Alpha|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|Kappa|Lambda|Mu|Nu|Xi|Omicron|Pi|Rho|Sigma|Tau|Upsilon|Phi|Chi|Psi|Omega)-[0-9]{3}$"))
+                {
+                    mission = value;
+                }
+                else
+                {
+                    Console.WriteLine("Mission name must contain the prefix Delta- followed by a 3 digit number");
+                }
+            }
+        }
+
+        public int Scoring
+        {
+            get { return scoring; }
+            set
+            {
+                if (value >= 0 && value <= 500)
+                {
+                    scoring = value;
+                }
+                else
+                {
+                    Console.WriteLine("Score must be between 0 and 500");
+                }
+            }
+        }
+
+        public int CompareTo(Score? other)
+        {
+            if (other == null) return 1;
+            return -Scoring.CompareTo(other.Scoring);
+        }
+
+        public override string ToString()
+        {
+            return "Player: " + Player + "\nMission: " + Mission + "\nScore: " + Scoring;
+        }
+
+    }
+}
